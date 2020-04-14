338dd9fd42dfa208a1ddd3c407fe4da41d86985f
WiZ3R <57489459+WiZ3R@users.noreply.github.com>
2020-04-14 14:22:17 +0200

    Add files via upload

---
+++ Gilles & john/php/unique/connexion.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gilles & John - Page de Connexion</title>
+</head>
+
+<body>
+
+    <main>
+
+    </main>
+
+</body>
+
+</html>
---
+++ Gilles & john/php/unique/entreprise.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gilles & John - l'Entreprise</title>
+</head>
+
+<body>
+    <?php include'header.php'?>
+
+    <main>
+
+    </main>
+
+    <?php include'footer.php'?>
+</body>
+
+</html>
