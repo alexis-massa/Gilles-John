bbf9d9102c0b132e1535f796cc9ce6b40d2deb52
Achot <achot.barseghyan@gmail.com>
2020-04-18 15:39:38 +0200

    add - Header
    not finished yet

---
+++ Gilles & john/php/commun/header.php
@@ -0,0 +1,19 @@
+<header>
+    <nav>
+        <img id="nav_logo" src="../../img/logo.png" alt="logo G&J">
+        <h1 id="nav_name">Gilles & John</h1>
+        <div class="nav-elements">
+            <a class="active" href="accueil.php">Accueil</a>
+            <div class="dropdown">
+                <button class="dropbtn">Produits</button>
+                <i class="fas fa-sort-down"></i>
+                <div class="dropdown-content">
+                    <a href="gilets.php">Gilets</a>
+                    <a href="accessoires.php">Accessoires</a>
+                </div>
+            </div>
+            <a href="entreprise.php">L'entreprise</a>
+            <a href="contact.php">Contact</a>
+        </div>
+    </nav>
+</header>
