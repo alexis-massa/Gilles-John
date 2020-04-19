7afd681513a37c64bc28e2ac0c518c86d108501b
Achot <achot.barseghyan@gmail.com>
2020-04-19 21:35:46 +0200

    Ajout des icones + panier
    + animations affichage panier

--- Gilles & john/php/commun/header.php
+++ Gilles & john/php/commun/header.php
@@ -5,15 +5,29 @@
         <div class="nav-elements">
             <a class="active" href="accueil.php">Accueil</a>
             <div class="dropdown">
-                <button class="dropbtn">Produits</button>
-                <i class="fas fa-sort-down"></i>
+                <button class="dropbtn">Produits <i class="fas fa-sort-down"></i></button>
                 <div class="dropdown-content">
-                    <a href="gilets.php">Gilets</a>
-                    <a href="accessoires.php">Accessoires</a>
+                    <a id="drop_gilets" href="gilets.php">Gilets</a>
+                    <a id="drop_access" href="accessoires.php">Accessoires</a>
                 </div>
             </div>
-            <a href="entreprise.php">L'entreprise</a>
-            <a href="contact.php">Contact</a>
+            <a id="entreprise" href="entreprise.php">L'entreprise</a>
+            <a id="contact" href="contact.php">Contact</a>
+            <i id="basket_shop" class="fas fa-shopping-basket"></i>
+            <i class="fas fa-search"></i>
+            <i class="fas fa-user"></i>
+        </div>
+        
+        <div id="container-shop-basket">
+            <div class="card card-body">
+                <label id="label-basket">panier</label>
+                <hr>
+                <div>
+                    <!-- ICI tous les produits ajoutés -->
+                </div>
+                <hr>
+                <span>Total: </span><span id="total-prix">0.00</span>
+            </div>
         </div>
     </nav>
 </header>
