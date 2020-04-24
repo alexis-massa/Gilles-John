d13e4f890da4a652bccc77ec69c0b780a1eeb281
iloxe <fifoualex73@gmail.com>
2020-04-24 09:05:39 +0200

    Update footer.php

--- Gilles & john/php/commun/footer.php
+++ Gilles & john/php/commun/footer.php
@@ -1,11 +1,20 @@
 <footer>
-    <div>
-    A PROPOS
+    <div id="propos">
+        <h3>A PROPOS</h3>
+        <p>Lorem ipsum dolor sit amet,
+            consectetur adipisicing elit, sed
+            do eiusmod tempor incididunt ut. </p>
     </div>
-    <div>
-    ADRESSE
+    <div id="adresse">
+        <h3>ADRESSE</h3>
+        <a href="tel:+33456781293">+33456781293</a>
+        <a href="mailto:email@email.com">email@email.com</a>
     </div>
-    <div>
-    INFORMATIONS
+    <div id="infos">
+        <h3>INFORMATIONS</h3>
+        <h4>Contactez-nous</h4>
+        <h4>Politique de confidentialité</h4>
+        <h4>Termes & conditions</h4>
+        <h4>Mentions légales</h4>
     </div>
 </footer>
