afd5b2495e462e4a04feaa13f31e885d9ee9ecbb
iloxe <fifoualex73@gmail.com>
2020-04-24 09:29:07 +0200

    Update footer.php

--- Gilles & john/php/commun/footer.php
+++ Gilles & john/php/commun/footer.php
@@ -1,20 +1,36 @@
 <footer>
-    <div id="propos">
-        <h3>A PROPOS</h3>
-        <p>Lorem ipsum dolor sit amet,
-            consectetur adipisicing elit, sed
-            do eiusmod tempor incididunt ut. </p>
-    </div>
     <div id="adresse">
-        <h3>ADRESSE</h3>
-        <a href="tel:+33456781293">+33456781293</a>
-        <a href="mailto:email@email.com">email@email.com</a>
     </div>
-    <div id="infos">
-        <h3>INFORMATIONS</h3>
-        <h4>Contactez-nous</h4>
-        <h4>Politique de confidentialité</h4>
-        <h4>Termes & conditions</h4>
-        <h4>Mentions légales</h4>
-    </div>
+    <table>
+        <tbody>
+            <tr>
+                <th>
+                    <h4>A PROPOS</h4>
+                </th>
+                <th>
+                    <h4>INFORMATIONS</h4>
+                </th>
+            </tr>
+            <tr>
+                <td>
+                    <p>Lorem ipsum dolor sit amet,
+                        consectetur adipisicing elit, sed
+                        do eiusmod tempor incididunt ut. </p>
+                </td>
+                <td>
+                    <a href="http://">Contactez-nous</a>
+                    <a href="http://">Politique de confidentialité</a>
+                    <a href="http://">Termes & conditions</a>
+                    <a href="http://">Mentions légales</a>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <h4>CONTACT</h4>
+                    <a id="footer-tel" href="tel:+33456781293">+33456781293</a>
+                    <a id="footer-mail" href="mailto:email@email.com">email@email.com</a>
+                </td>
+            </tr>
+        </tbody>
+    </table>
 </footer>
