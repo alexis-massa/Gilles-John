59bcfb48c911733978b67fb4c9ec54266d434e5e
baptiste7730 <baptiste.fleck30gmail.com>
2020-04-26 15:14:35 +0200

    J'ai remis les fichiers comme il faut

--- Gilles & john/php/unique/mention_legale.php
+++ Gilles & john/php/unique/mention_legale.php
@@ -15,12 +15,66 @@
     integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="../css/header.css">
     <link rel="stylesheet" href="../../css/footer.css">
+    <link rel="stylesheet" href="../../css/main.css">
 </head>
 
 <body>
     <?php include '../commun/header.php' ?>
 
     <main>
+        <h2>Mentions légales</h2>
+
+            <ol>
+                <li><p>Ce site internet est édité et hébergé par Gilles & John, société anonyme au capital de 1 842 972,28 €, immatriculée au Registre du Commerce et des Sociétés de Lyon sous le numéro 012 345 678, dont le siège social est situé 952 route du pont romain, 73360 Saint-Christophe-la-Grotte, numéro TVA intracommunautaire FR 34 123 456 789. </p><br><p>Le directeur de la publication est Monsieur Alexis MASSA, Président Directeur Général.</p></li>
+                <li><p>Pour toute information ou conseil, vous pouvez nous contacter : </p></li>
+                    <ul type='circle'>
+                        <li>Par téléphone, via notre Ligne Conseil depuis : 01 23 45 67 89</li>
+                        <li>Notre service client depuis l’adresse suivante : Service.client@Gilles-&-John</li>
+                    </ul>
+                <li><p>Gilles & John met tout en œuvre pour offrir à ses utilisateurs la meilleure expérience de navigation possible ainsi que des informations complètes et pertinentes. Dans le cas où vous remarqueriez un contenu inapproprié ou illicite, nous vous invitons à nous le signaler à l’adresse suivante : direction.marketing@Gilles-&-John.com </p></li>
+                <li><p>Gilles & John est propriétaire et/ou dispose des autorisations nécessaires pour diffuser l’intégralité du contenu du Site ou rendu disponible à travers le Site et notamment, les textes, dessins, graphiques, images, photos, marques et logos. Ces contenus sont protégés par des droits de propriété intellectuelle ; il vous est par conséquent interdit de copier, modifier, retranscrire, extraire, réutiliser et d’une manière générale reproduire et diffuser lesdits contenus sans l’autorisation expresse de Gilles & John. Nous attirons votre attention sur le fait que la violation des droits de propriété intellectuelle est constitutive du délit de contrefaçon, pénalement sanctionné.</p></li>
+                <li><p>Afin d’avoir plus d’informations sur la protection et le traitement de vos données à caractère personnel ainsi que sur les cookies, nous vous invitons à consulter notre page « Politique de confidentialité » accessible au bas de chaque page du Site.</p></li>
+            </ol>
+
+
+        <h2>Conditions générales de vente et d'utilisations </h2>
+
+            <ol>
+                <li><h4>Acceptation</h4></li>
+                    <p>L’information et le contenu proposés dans le site Web sont fournis sous réserve des termes et conditions suivantes. Votre accès à ce site Web et son utilisation constitue une acceptation complète de ces termes et conditions.</p>
+                <li><h4>Objet du site web</h4></li>
+                    <p>Le présent site est destiné à fournir des informations relatives à la société Gilles & John, ses filiales et sociétés affiliées, son organisation, les produits et services offerts par ses divisions et la performance de ses actions. Cependant, ces informations ne représentent qu’une présentation des produits et services de Gilles & John et ne sont pas contractuelles. Pour conclure toute forme d’accord contractuel, veuillez contacter directement la société mère ou l’une de ses filiales.</p>
+                <li><h4>Garanties et responsabilités</h4></li>
+                    <p>Les informations fournies sur ce site sont gratuites et proposées uniquement à titre informatif ; elles ne créent pas de relation commerciale ou professionnelle entre Gilles & John et vous. Votre utilisation des informations fournies sur ce site, ou de tout site ou service lié à ce site, est de votre responsabilité.</p>
+                    <p>Bien que Gilles & John mette tout en œuvre pour inclure des informations exactes et à jour sur ce site Web, il peut tout de même contenir des inexactitudes, des informations obsolètes ou des erreurs typographiques. Par conséquent, Gilles & John ne donne aucune garantie formelle quant à l’exactitude ou l’exhaustivité de toute information contenue dans ce document. De plus, Gilles & John n’assume aucune responsabilité, obligation ou engagement pour toute erreur ou omission dans le contenu de ce site.</p>
+                    <p>L’utilisation des éléments et informations de ce site Web sont de la seule responsabilité de l’utilisateur.</p>
+                    <p>Gilles & John décline toute responsabilité pour tout dommage, y compris les dommages indirects, consécutifs ou accessoires ; y compris mais sans s’y limiter, la perte de profits ou de revenus, l’interruption des activités et la perte de données découlant de ou en relation avec l’utilisation, l’incapacité à utiliser ou la confiance accordée à tout contenu de ce site ou à tout site lié pouvant résulter de l’accès à ce site et de l’utilisation totale ou partielle des éléments ou des informations qui y sont contenues.</p>
+                    <p>Gilles & John ne garantit en aucun cas l’accès continu au site ainsi que la sécurité du site et l’absence de virus ou d’autres hôtes indésirables (y compris dans les éléments du site et dans les informations qui y sont incorporées).</p>
+                <li><h4>Modification</h4></li>
+                    <p>Gilles & John peut modifier et mettre à jour à tout moment, et sans préavis, les contenus ou toute autre information publiés sur ce site.</p>
+                <li><h4>Sites web tierces</h4></li>
+                    <p>Ce site Web fournit des liens vers des sites Web de tiers qui ne sont pas gérés ou sous le contrôle de Gilles & John. Ces liens sont fournis pour votre commodité et n’impliquent pas l’approbation ou la validation par Gilles & John des sites liés. Gilles & John n’a pas examiné tout ou partie des sites liés au sien et n’est donc pas responsable de leur contenu ni des pages hors site ou de tout autre site lié à ces sites tierces.</p>
+                <li><h4>Téléchargement de document sur le site web</h4></li>
+                    <p>Toute information ou demande d’information formulée auprès de Gilles & John, via le site web ou par e-mail auquel le site peut donner accès, est considérée comme non confidentielle. Gilles & John n’assume aucune obligation de surveiller les informations que vous pouvez publier sur son site Web.</p>
+                    <p>En soumettant toute information ou matériel, vous donnez à la Société une licence illimitée et irrévocable pour utiliser, exécuter, montrer, modifier et transmettre ces informations, matériels ou commentaires, y compris toute idée sous-jacente, concept ou savoir-faire (le terme « matériel » est destiné à couvrir tous les projets, fichiers ou autres pièces jointes qui nous sont envoyés). La Société se réserve le droit d’utiliser ces informations de quelque manière que ce soit.</p>
+                    <p>Gilles & John se réserve le droit en tout temps et à sa seule discrétion, d’examiner le contenu soumis et de le modifier, déplacer, supprimer et/ou refuser si, selon le jugement de Gilles & John, ce contenu viole ces conditions d’utilisation ou est autrement inacceptable ou inapproprié, que ce soit pour des raisons juridiques ou autres. Vous garantissez que toute information, matériel, projet, fichier, pièce jointe ou commentaire qui nous est envoyé autre que des données personnelles, et transmis à Gilles & John par le biais du site Web ne viole pas les droits de propriété intellectuelle ou toute autre loi applicable. Ces informations seront traitées comme non confidentielles. En soumettant du matériel sur ce site Web, vous accordez à Gilles & John, et à toute personne autorisée par Gilles & John, une licence mondiale libre de droits, perpétuelle, irrévocable, non exclusive et illimitée pour utiliser, copier, modifier, transmettre, vendre, exploiter, créer des œuvres dérivées de, distribuer, et/ou exécuter publiquement ou afficher ce matériel, en tout ou en partie, de quelque manière ou moyen (qu’il soit maintenant connu ou développé ci-après), et à toute fin choisie par Gilles & John.</p>
+                <li><h4>Loi applicable</h4></li>
+                    <p>Dans le cas où une partie de ces Termes et Conditions est jugée inapplicable, la partie inapplicable doit être interprétée conformément à la loi applicable aussi près que possible pour refléter les intentions d’origine des parties, et le reste des dispositions doit rester pleinement applicable.</p>
+                    <p>Le manquement de Gilles & John à appliquer ou insister sur l’exécution stricte de toute disposition du présent contrat ne doit pas être interprété comme une renonciation à quelque disposition ou droit que ce soit. Ni la conduite des parties mentionnées ici, ni la pratique commerciale ne doit agir pour modifier toute disposition de ces Termes et Conditions.</p>
+                    <p>Ces Termes et Conditions seront régis et interprétés conformément à la loi française. Toute cause d’action contre Gilles & John doit être engagée en France.</p>
+                <li><h4>Communiqués de presse</h4></li>
+                    <p>Les communiqués de presse de Gilles & John sont présentés sur ce site uniquement à des fins historiques. Les informations fournies par Gilles & John étaient exactes au moment de la publication, sous réserve de la clause de non-responsabilité relative aux déclarations prospectives, qui a été imprimée en même temps que l’annonce. Les investisseurs ne doivent pas supposer que les déclarations faites dans ces documents restent en vigueur à une date ultérieure. Gilles & John n’examine pas les publications passées pour déterminer si elles restent exactes, et les informations contenues dans ces publications peuvent être remplacées. Gilles & John décline toute obligation de mettre à jour les déclarations ou les communiqués de presse. Les informations financières accessibles via cette page ne sont valables qu’à la date ou aux dates de leur création. Ces informations peuvent être obsolètes. Gilles & John n’assume aucune responsabilité et décline toute obligation de mettre à jour les informations disponibles sur cette page. La performance financière future de Gilles & John est soumise à diverses tendances, risques et incertitudes qui pourraient faire en sorte que les résultats réels diffèrent sensiblement de ceux exprimés dans les énoncés prospectifs. Les tendances, risques et incertitudes susceptibles d’affecter les résultats financiers futurs de Gilles & John sont discutés plus en détail dans les rapports de Gilles & John déposés auprès de l’AMF, y compris le rapport annuel de Gilles & John pour l’exercice le plus récent.</p>
+                <li><h4>Avis de droits de propriété intellectuelle</h4></li>
+                    <p>Ce site est protégé par des droits de propriété intellectuelle et est la propriété exclusive de Gilles & John. Tout matériel qu’il contient, y compris, mais sans s’y limiter, les textes, graphiques, informations, motifs, logos, designs et bases de données, images, sons, vidéos, logos, icônes ou code html est protégé par la loi sur la propriété intellectuelle et reste la propriété de Gilles & John ou de la partie tierce.</p>
+                    <p>Vous pouvez utiliser ce matériel à des fins personnelles et non commerciales conformément aux principes régissant le droit de la propriété intellectuelle. Toute autre utilisation ou modification du contenu du site Web de la Société sans l’autorisation écrite préalable de la Société est interdite.</p>
+                    <p>Sauf mention contraire expresse, tous les droits de propriété intellectuelle contenus dans ce site, y compris les noms de domaine, l’organisation et la mise en page du site et les logiciels utilisés en relation avec le site sont la propriété de Gilles & John.</p>
+                    <p>Vous ne pouvez pas reproduire ou stocker une partie de ce site dans un autre site Web, un système de gestion de documents ou un système de récupération électronique (par copier-coller ou autre) sans l’autorisation écrite préalable de Gilles & John. Vous ne devez utiliser aucune partie du contenu de ce site à des fins commerciales sans obtenir une licence de Gilles & John.</p>
+                    <p>Si vous imprimez, copiez ou téléchargez une partie de ce site en violation de ces conditions d’utilisation, votre droit d’utiliser ce site cessera immédiatement et vous devrez, à la demande de Gilles & John, retourner ou détruire toute copie des documents que vous auriez fait. Tous les droits non expressément accordés dans cet avis sont réservés.</p>
+                <li><h4>Protection des données et confidentialité</h4></li>
+                    <p>Consulter notre politique de confidentialité</p>
+                <li><h4>Modification des ces conditions générales de vente et d'utilisation</h4></li>
+                    <p>Gilles & John peut modifier à tout moment et sans préavis ces conditions d’utilisation et vous encourage donc à les lire régulièrement.</p>
+            </ol>
 
     </main>
 
--- Gilles & john/php/unique/politique_de_confidentialite.php
+++ Gilles & john/php/unique/politique_de_confidentialite.php
@@ -15,12 +15,109 @@
     integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="../css/header.css">
     <link rel="stylesheet" href="../../css/footer.css">
+    <link rel="stylesheet" href="../../css/main.css">
 </head>
 
 <body>
     <?php include '../commun/header.php' ?>
 
     <main>
+        <h1> Politique de confidentialité </h1>
+
+        <ol>
+            <li><h4> Qu’est-ce qu’une donnée personnelle ? </h4></li>
+
+                <p>Une donnée personnelle se définit comme toute information relative à une personne physique identifiée ou identifiable. Une personne physique identifiable est une personne qui peut être identifiée, directement ou indirectement, notamment par référence à un identifiant tel qu’un nom, un numéro d’identification, des données de localisation, un identifiant en ligne ou à un ou plusieurs facteurs spécifiques à cette personne physique.</p>
+
+    
+            <li><h4> Comment sont collectées vos données personnelles ?  </h4></li>
+
+                <p>Vos données personnelles peuvent être collectées directement ou indirectement. Gilles & John, en tant que responsable de traitement peut collecter des données personnelles vous concernant lorsque vous : </p>
+
+            <ul>
+                <li>	Souscrivez volontairement à la newsletter Gilles & John ; </li>
+                <li>	Remplissez le formulaire de contact ;  </li>
+                <li>	Avez participé à des évènements dans lesquels Gilles & John était présent ;  </li>
+                <li>	Echangez avec les équipes commerciales de Gilles & John ; </li>
+            </ul>
+
+                <p>L’ensemble de ces traitements reposent sur votre consentement. Dès lors, vous pouvez à tout moment demander à Gilles & John de cesser le traitement de vos données personnelles dans les conditions prévues ci-après.</p>
+
+                <p>Vos données personnelles peuvent être collectées indirectement lors de l’utilisation de cookies et d’autres technologies lorsque vous consultez le site internet www. Gilles&John.fr.</p>
+
+    
+            <li><h4> Où sont traitées vos données personnelles ?  </h4></li>
+
+                <p>Parce que la protection des données personnelles et la sécurité sont importantes, Gilles & John a adopté des règles contraignantes d’entreprise (ci-après « RCE »)</p>
+
+                <p>Gilles & John a obtenu l’approbation de ses RCE par la Cnil, à la fois en tant que responsable de traitement et en tant que sous-traitant.</p>
+
+                <p>Cette approbation témoigne de l’engagement de Gilles & John à protéger non seulement ses propres données personnelles mais aussi celles de ses clients, de ses partenaires, ou de ses contacts. Les RCE constituent un engagement par lequel les entités du Groupe Gilles & John s’engagent à traiter les données personnelles en accord avec un niveau élevé de protection des données personnelles qu’elles traitent pour leurs propres besoins mais aussi pour les besoins de leurs clients, permettant ainsi la libre circulation des données personnelles entre les entités du Groupe Gilles & John. Le site internet de Gilles & John est hébergé dans l’Union Européenne et le support applicatif peut être réalisé auprès de la société Gilles & John. Les transferts résultant d’un tel support sont encadrés par les RCE.</p>
+
+    
+            <li><h4> Qui peut accéder à vos données personnelles ?  </h4></li>
+
+                <p>Vos données personnelles peuvent être communiquées aux personnes suivantes : </p>
+
+                <ul>
+                    <li>	En interne, aux collaborateurs ayant été strictement autorisés individuellement à y accéder ; </li>
+                    <li>	À nos sous-traitants internes, ou externes, le cas échéant ; </li>
+                    <li>	À nos partenaires lorsque nous faisons notre offre avec des tiers.</li>
+                </ul>
+
+
+            <li><h4> Quelles mesures mettons-nous en œuvre pour protéger vos données personnelles ?  </h4></li>
+
+                <p>Toutes les données personnelles traitées par Gilles & John sont conservées sur des serveurs sécurisés. Gilles & John met en œuvre des mesures administratives, techniques, personnelles et physiques appropriées pour protéger les données personnelles qu’elle traite contre la perte, l’accès, l’utilisation, la divulgation ou la modification non-autorisés et pour garantir l’intégrité des données personnelles. De plus, Gilles & John assure le traitement de vos données personnelles uniquement aux fins mentionnées dans la présente notice d’information relative aux données personnelles et ne conserve pas les données plus longtemps que nécessaire. A titre d’information, Gilles & John conserve les données pendant trois ans à compter du dernier contact que Gilles & John a eu avec vous.</p>
+
+
+            <li><h4> Quels sont vos droits ?  </h4></li>
+
+                <p>Vous avez le droit de demander l’accès, la rectification, ou l’effacement, la communication d’un format portable de vos données personnelles. Vous pouvez également limiter ou vous opposer au traitement de vos données personnelles. Par ailleurs, vous disposez d’un droit de définir des directives générales et/ou particulières relatives au sort de vos données personnelles et à la manière dont vous souhaitez que vos droits soient exercés après votre décès. A cet égard, en cas de décès qui serait porté à notre connaissance, vos données seront supprimées, sauf nécessité de conservation pendant une durée déterminée pour des motifs tenant à nos obligations légales et réglementaires et/ou aux délais légaux de prescription, et après le cas échéant avoir été communiquées à un tiers éventuellement désigné par vos soins.</p>
+
+                <p>Vous disposez également du droit de déposer une réclamation auprès de la Cnil.</p>
+
+                <p>Gilles & John a nommé un délégué à la protection des données personnelles que vous pouvez contacter en lui adressant un message à l’adresse suivante : <br> Sarah.Pail@Gilles-&-John.com</p>
+
+                <p>Cette notice d’information a été modifiée le 27 mars 2019 et est mise à jour régulièrement.</p>
+        </ol>
+
+
+
+        <h1> Politique cookies </h1>
+
+        <p>Gilles & John dépose sur son site internet www.Gilles&John.fr des petits fichiers au format alphanumérique permettant de reconnaitre les utilisateurs et d’améliorer notamment leur expérience de navigation. En tant qu’utilisateur du site vous trouverez donc ci-dessous les informations sur les traitements que nous réalisons. Les cookies peuvent avoir différents objectifs et fonctionnalités. Pour plus d’informations sur les cookies, visitez les sites internet suivants : www.allaboutcookies.org et www.youronlinechoices.eu</p>
+
+        <ol>
+            <li><h4> Quels sont les types de cookies déposés par Gilles & John ?  </h4></li>
+            
+                <p> Gilles & John peut déposer sur votre appareil, quatre types de cookies :  </p>
+                <ul>
+                    <li> 1.	Cookies fonctionnels :  </li> <p>ces cookies sont essentiels à votre navigation et vous permettent d’utiliser les principales fonctionnalités du Site et de sécuriser votre connexion.</p>
+                    <li> 2.	Cookies de « performance » :  </li> <p>ces cookies recueillent des informations sur votre utilisation du site internet, telles que les pages internet visitées et les messages d’erreur rencontrés ; ils ne collectent pas d’informations personnellement identifiables, et les informations collectées sont agrégées de telle sorte qu’elles sont anonymes. Les cookies de performance sont utilisés pour améliorer le fonctionnement d’un site internet.</p>
+                    <li> 3.	Cookies de ciblage publicitaire :  </li> <p>ces cookies sont utilisés pour diffuser des publicités personnalisées selon votre profil et en limiter le nombre d’affichages, aider à mesurer l’efficacité de la campagne de publicité, et comprendre le comportement des gens après avoir vu une publicité.</p>
+                    <li> 4.	Cookies des réseaux sociaux :  </li> <p>nous vous permettons de partager du contenu de notre Site sur les réseaux sociaux via des boutons « Partager », « J’aime », issus de réseaux sociaux tels que « Facebook », « Twitter », etc. En partageant le contenu, vous permettez au réseau social concerné de suivre votre navigation sur nos Sites et/ou applications du seul fait que votre compte au réseau social concerné était activé sur votre appareil (session ouverte) durant votre navigation sur notre Site. Nous n’avons aucun contrôle sur le processus employé par les réseaux sociaux pour collecter des informations relatives à votre navigation. Nous vous invitons à consulter les politiques de protection de la vie privée de ces réseaux sociaux afin de prendre connaissance des finalités d’utilisation, notamment publicitaires, des informations de navigation qu’ils peuvent recueillir grâce à ces boutons.</p>
+                </ul>
+        
+
+            <li><h4> Quel est le fondement de la collecte des cookies ?  </h4></li>
+            
+                <p>L’enregistrement d’un cookie est essentiellement subordonné à la volonté de l’utilisateur, que celui-ci peut exprimer et modifier à tout moment et gratuitement dans les conditions prévues ci-dessous.</p>
+
+                <p>Si vous refusez l’enregistrement de cookies, ou si vous supprimez ceux qui y sont enregistrés, vous ne pourrez plus bénéficier de toutes les fonctionnalités de notre Site. Le cas échéant, nous déclinons toute responsabilité pour les conséquences liées au fonctionnement dégradé de nos services résultant de l’impossibilité pour nous d’enregistrer ou de consulter les cookies nécessaires à leur fonctionnement et que vous auriez refusés ou supprimés.</p>
+        
+
+            <li><h4> Comment puis-je contrôler ou supprimer les cookies ?  </h4></li>
+
+                <p>La plupart des navigateurs sont initialement configurés pour accepter automatiquement les cookies. Vous pouvez modifier les paramètres pour bloquer les cookies ou être alerté lorsque des cookies sont envoyés sur votre appareil. Il existe plusieurs façons de gérer les cookies. Vous pouvez effectivement ajuster ou modifier les paramètres de votre navigateur pour cesser le dépôt de cookies sur votre appareil.</p>
+
+                <ul type='circle'>
+                    <li><a href='https://support.microsoft.com/en-us/help/17442/windows-internet-explorer-delete-manage-cookies' target='_blank'>	Paramètres de cookies dans Internet Explorer</a></li>
+                    <li><a href="https://support.mozilla.org/en-US/kb/cookies-information-websites-store-on-your-computer?redirectlocale=en-US&redirectslug=Cookies" target='_blank'>	Paramètres de cookies dans Firefox</a></li>
+                    <li><a href="https://support.google.com/chrome/answer/95647?hl=en" target='_blank'>   Paramètres de cookie dans Chrome</a></li>
+                    <li>   Paramètres de cookies dans Safari</li>	
+                </ul>
+        </ol>   
 
     </main>
 
