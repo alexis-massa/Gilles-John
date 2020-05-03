5fc6a7005ee1ab3d936787867911e7fbd091287b
iloxe <fifoualex73@gmail.com>
2020-05-03 12:13:26 +0200

    Update gilets.php
    Ca marche un peu, mais c'est pas ce qu'on veut et c'est moche, mais ca avance

--- Gilles & john/php/unique/gilets.php
+++ Gilles & john/php/unique/gilets.php
@@ -9,6 +9,7 @@
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+    <script src="https://kit.fontawesome.com/63ffd472bc.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="../../css/header.css">
     <link rel="stylesheet" href="../../css/footer.css">
     <link rel="stylesheet" href="../../css/cart.css">
@@ -28,8 +29,7 @@
             //Connection par PDO pour les requêtes préparées
             $db_pdo = new PDO("pgsql:host=localhost; dbname=PPE_Groupe5", "postgres", "postgre");
 
-            $req_produits = 'SELECT * FROM produit ORDER BY id_prod ASC';
-
+            $req_produits = 'SELECT * FROM produit INNER JOIN image ON image.id_img = produit.id_img INNER JOIN stock ON stock.id_prod = produit.id_prod ORDER BY produit.id_prod ASC;';
 
 
 
@@ -42,19 +42,17 @@
                     //On parcourt toutes les lignes et on les ajoute dans un tableau associatif
                     while ($produit = pg_fetch_assoc($result)) {
                         //Opérations sur la requête ici :
-                        $req_images = $db_pdo->prepare('SELECT chemin_img FROM produit INNER JOIN image on image.id_img = produit.id_img WHERE id_prod = ?');
-                        $req_images->execute([$produit['id_img']]);
-                        $image = pg_fetch_assoc($req_images);
-
-
-
             ?>
 
                         <div class="col-sm-4 col-md-3">
                             <form method="POST" action="gilets.php?action=add&id=<?php echo $produit['id_prod']; ?>">
                                 <div class="products">
-                                    <img src="<?php $image ?>" class="img-responsive">
-                                    <h4 class="text-info"></h4>
+                                    <img src="<?php echo $produit['chemin_img']; ?>" class="img-responsive">
+                                    <h4 class="text-info"><?php echo $produit['nom_prod']; ?></h4>
+                                    <h4><?php echo $produit['prix_vente']; ?></h4>
+                                    <input type="text" name="quantity" class="form-control" value="1">
+                                    <input type="hidden" name="name" class="form-control" value="<?php echo $produit['nom_prod']; ?>">
+                                    <input type="hidden" name="price" class="form-control" value="<?php echo $produit['prix_vente']; ?>">
                                 </div>
                             </form>
                         </div>
