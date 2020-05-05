4245e751a201122d6a12eb48d23bdc61ffa01a69
iloxe <fifoualex73@gmail.com>
2020-05-05 16:22:00 +0200

    Update gilets.php

--- Gilles & john/php/unique/gilets.php
+++ Gilles & john/php/unique/gilets.php
@@ -29,12 +29,8 @@
             //Connection par PDO pour les requêtes préparées
             $db_pdo = new PDO("pgsql:host=localhost; dbname=PPE_Groupe5", "postgres", "postgre");
 
-            //$req_produits = 'SELECT * FROM produit INNER JOIN image ON image.id_img = produit.id_img INNER JOIN stock ON stock.id_prod = produit.id_prod ORDER BY produit.id_prod ASC;';
+            
             $req_produits = 'SELECT * FROM produit ORDER BY produit.id_prod ASC;';
-
-
-
-
 
             $req_image = $db_pdo->prepare('SELECT chemin_img FROM image WHERE id_img = ?');
             $idImg = '';
