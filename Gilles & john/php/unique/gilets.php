d87c98a725aef72f1871324f742b404bd05b4f4a
iloxe <fifoualex73@gmail.com>
2020-05-05 16:18:30 +0200

    update de requete image
    Ca marche

--- Gilles & john/php/unique/gilets.php
+++ Gilles & john/php/unique/gilets.php
@@ -29,10 +29,19 @@
             //Connection par PDO pour les requêtes préparées
             $db_pdo = new PDO("pgsql:host=localhost; dbname=PPE_Groupe5", "postgres", "postgre");
 
-            $req_produits = 'SELECT * FROM produit INNER JOIN image ON image.id_img = produit.id_img INNER JOIN stock ON stock.id_prod = produit.id_prod ORDER BY produit.id_prod ASC;';
+            //$req_produits = 'SELECT * FROM produit INNER JOIN image ON image.id_img = produit.id_img INNER JOIN stock ON stock.id_prod = produit.id_prod ORDER BY produit.id_prod ASC;';
+            $req_produits = 'SELECT * FROM produit ORDER BY produit.id_prod ASC;';
 
 
 
+
+
+            $req_image = $db_pdo->prepare('SELECT chemin_img FROM image WHERE id_img = ?');
+            $idImg = '';
+            $req_image->bindParam(1, $idImg);
+            
+            
+            
             // Résultat de la requète dans une variable
             $result = pg_exec($db_connection, $req_produits);
             //S'il y a un résultat (pas d'erreur dans la requete)
@@ -42,24 +51,30 @@
                     //On parcourt toutes les lignes et on les ajoute dans un tableau associatif
                     while ($produit = pg_fetch_assoc($result)) {
                         //Opérations sur la requête ici :
+                                                
+                        echo $produit['id_img'];
+                        $idImg = $produit['id_img'];
+                        $req_image->execute();
+
+                       $image = $req_image->fetch();
+
             ?>
 
                         <div class="col-sm-4 col-md-3">
                             <form method="POST" action="gilets.php?action=add&id=<?php echo $produit['id_prod']; ?>">
                                 <div class="products">
-                                    <img src="<?php echo $produit['chemin_img']; ?>" class="img-responsive">
+                                    <img src="<?php echo $image['chemin_img']; ?>" class="img-responsive">
                                     <h4 class="text-info"><?php echo $produit['nom_prod']; ?></h4>
                                     <h4><?php echo $produit['prix_vente']; ?></h4>
                                     <input type="text" name="quantity" class="form-control" value="1">
                                     <input type="hidden" name="name" class="form-control" value="<?php echo $produit['nom_prod']; ?>">
-                                    <input type="hidden" name="price" class="form-control" value="<?php echo $produit['prix_vente']; ?>">
+                                    <input type="hidden" name="price" class="form-control" value="<?php //echo $produit['prix_vente']; ?>">
                                     <input type="submit" name="add_to_cart" class="btn btn-info" value="Ajouter au panier">
                                 </div>
                             </form>
                         </div>
 
             <?php
-                        //Requete chemin image (test):
                     }
                 }
             }
