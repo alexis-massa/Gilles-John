c33a848ecdcdc5d68b285fd2483bca14a69929d5
iloxe <fifoualex73@gmail.com>
2020-05-15 17:52:38 +0200

    SSelection couleur / taille
    Un nouveau départ

--- Gilles & john/js/magasin.js
+++ Gilles & john/js/magasin.js
@@ -1,10 +1,28 @@
-$(".color").click(function(){
+$(".color").click(function () {
     $(".products span").removeClass("activeColor");
     $(this).addClass("activeColor");
-    $(".product-pic").css("background-image", $(this).attr("data-pic"));
 });
-$(".taille").click(function(){
+$(".taille").click(function () {
     $(".products span").removeClass("activeTaille");
     $(this).addClass("activeTaille");
     $(".prix").html($(this).attr("data-prix"));
 });
+
+
+var grd = function () {
+    $("input[type='radio']").click(function () {
+        var previousValue = $(this).attr('previousValue');
+        var name = $(this).attr('name');
+
+        if (previousValue == 'checked') {
+            $(this).removeAttr('checked');
+            $(this).attr('previousValue', false);
+        } else {
+            $("input[name=" + name + "]:radio").attr('previousValue', false);
+            $(this).attr('previousValue', 'checked');
+        }
+    });
+};
+
+grd('radio_coul');
+grd('radio_taille');
